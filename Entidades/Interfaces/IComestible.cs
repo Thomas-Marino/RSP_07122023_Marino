f97399599dbdad27d2cc43cff84d3af4e7972436
Thomas-Marino <thomasmarino627@gmail.com>
2023-12-03 16:13:40 -0300

    Implementacion de interfaces

--- Entidades/Interfaces/IComestible.cs
+++ Entidades/Interfaces/IComestible.cs
@@ -2,7 +2,11 @@
 {
     public interface IComestible
     {
+        public bool Estado { get; }
+        public string Imagen { get; }
+        public string Ticket { get; }
 
-
+        public void FinalizarPreparacion(string cocinero);
+        public void IniciarPreparacion();
     }
 }
