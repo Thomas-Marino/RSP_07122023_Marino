b030076dd7c2eb5e4775a2660e209f33cf06de25
Thomas-Marino <thomasmarino627@gmail.com>
2023-12-03 14:23:06 -0300

    Primer push

---
+++ Entidades/Archivos/FileManager.cs
@@ -0,0 +1,24 @@
+﻿using Entidades.Exceptions;
+using Entidades.Interfaces;
+using Entidades.Modelos;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Entidades.Files
+{
+    
+    public static class FileManager
+    {
+        private static string path;
+
+        private static FileManager()
+        {
+
+        }
+    }
+}
