ab3d6e2548934edbeddf42e2a2520aaa2f7685e5
Thomas-Marino <thomasmarino627@gmail.com>
2023-12-03 16:51:40 -0300

    Implementacion de metodo de extension

--- Entidades/MetodosDeExtension/IngredientesExtension.cs
+++ Entidades/MetodosDeExtension/IngredientesExtension.cs
@@ -3,9 +3,35 @@
 
 namespace Entidades.MetodosDeExtension
 {
-    public class IngredientesExtension
+    public static class IngredientesExtension
     {
 
+        public static double CalcularCostoIngredientes(List<EIngrediente> ingredientes, int costoInicial)
+        {
+            double precioFinal = 0.0;
+
+            foreach (var ingrediente in ingredientes)
+            {
+                int precioIngrediente = (int)ingrediente;
+                int incremento = costoInicial * precioIngrediente / 100;
+                precioFinal += incremento;
+            }
+
+            return precioFinal;
+        }
+        public static List<EIngrediente> IngredientesAleatorios(Random rand)
+        {
+            List<EIngrediente> ingredientes = new List<EIngrediente>()
+            {
+                EIngrediente.ADHERESO,
+                EIngrediente.QUESO,
+                EIngrediente.JAMON,
+                EIngrediente.HUEVO,
+                EIngrediente.PANCETA
+            };
+            int numeroAleatorio = rand.Next(1, (ingredientes.Count + 1));
+            return ingredientes.Take(numeroAleatorio).ToList();
+        }
 
     }
 }
